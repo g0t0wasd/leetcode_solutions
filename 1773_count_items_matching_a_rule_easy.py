01604718e558320100f00b42638ae6881505a5d4
Dima Krasota <dmytro.krasota@socialsweethearts.de>
2022-09-07 21:25:30 +0200

    1773 Count Items Matching a Rule | Easy

--- 1773_count_items_matching_a_rule_easy.py
+++ 1773_count_items_matching_a_rule_easy.py
@@ -3,6 +3,7 @@ from typing import List
 
 class Solution:
     def countMatches(self, items: List[List[str]], ruleKey: str, ruleValue: str) -> int:
+        ''' Test '''
         rules = ('type', 'color', 'name')
         total_count = 0
         index = rules.index(ruleKey)
